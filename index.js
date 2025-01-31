d2d13d20c82db742152db73861f77065bf9344cd
Dark-Mart <atilanorush@gmail.com>
2025-01-31 20:24:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
     let riveContainer = document.getElementById("riveContainer");
-    let isMobile = window.innerWidth <= 768; // Detectamos si es móvil
 
-    // Código embed de Rive según el dispositivo
-    let riveEmbed = isMobile 
-        ? '<iframe src="https://rive.app/s/_m9nvIj_sEizaP2qmQWzPA/embed" allowfullscreen allow="autoplay"></iframe>' 
-        : '<iframe src="https://rive.app/s/VkjyCngtz0qPt_8DR1KFWw/embed" allowfullscreen allow="autoplay"></iframe>';
+    function loadRive() {
+        let isMobile = window.innerWidth <= 768;
+        let riveEmbed = isMobile 
+            ? '<iframe width="100%" height="100%" src="https://rive.app/s/_m9nvIj_sEizaP2qmQWzPA/embed" allowfullscreen allow="autoplay"></iframe>' 
+            : '<iframe width="100%" height="100%" src="https://rive.app/s/VkjyCngtz0qPt_8DR1KFWw/embed" allowfullscreen allow="autoplay"></iframe>';
 
-    riveContainer.innerHTML = riveEmbed;
+        riveContainer.innerHTML = riveEmbed;
+    }
+
+    // Cargar la animación al inicio
+    loadRive();
+
+    // Volver a cargar si cambia el tamaño de la pantalla
+    window.addEventListener("resize", function() {
+        loadRive();
+    });
 });
